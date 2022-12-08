30ff38996ea8e35e7aa7cd88316a11c376ee8074
ehsanghaffarii <ghafari.5000@gmail.com>
2022-12-08 05:29:57 +0330

    chaining

---
+++ part-2/Chaining-Multiple-Conditions.py
@@ -0,0 +1,7 @@
+x = 4
+y = 2
+if not 1 + 1 == y or x == 4 and 7 == 8:
+  print("Yes")
+elif x > y:
+  print("No")
+
---
+++ part-2/list.py
@@ -0,0 +1,17 @@
+# fruits = ["apple", "cherry", "banana", "kiwi", "lemon", "pear", "peach", "avocado"]
+# num = int(input())
+
+
+# if (num < 0 or num > 7):
+#   print("Wrong number")
+# else:
+#   n = fruits[num]
+#   print(n)
+
+
+
+
+num = [5, 4, 3, [2], 1]
+print(num[0])
+print(num[3][0])
+print(num[5])
